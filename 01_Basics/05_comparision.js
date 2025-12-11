27f96df9a4c3c84a84062e38017af1d6a0e99802
HARSH DUBEY <amanad08052@gmail.com>
2025-12-12 00:22:07 +0530

    updated logics for comparision js

---
+++ 01_Basics/05_comparision.js
@@ -0,0 +1,18 @@
+// console.log("2"> 1);
+// console.log("02"> 1);
+
+// console.log(null > 0);false
+// console.log(null == 0);false
+// console.log(null >= 0);true
+
+// all false
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+
+// **** === check both value and data type ****
+
+// console.log("2" == 2);
+// console.log("2" === 2);
+// console.log(2 === 2);
