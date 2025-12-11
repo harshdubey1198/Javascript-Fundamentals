740863eb1e15c8dfc3ac956799d173ba9ec78ed4
HARSH DUBEY <amanad08052@gmail.com>
2025-12-11 23:40:15 +0530

    type conversion added

---
+++ 01_Basics/04_conversionDatatypes.js
@@ -0,0 +1,20 @@
+let score = true;
+
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0 
+
+let isUser = 1
+
+let booleanIsUser =   Boolean(isUser);
+console.log(booleanIsUser);
+
+//  ""=> false
+//  "harsh"=> true
